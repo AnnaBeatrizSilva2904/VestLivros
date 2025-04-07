49eb9a76c492c2ce2b0c341af337a9188ec5214e
AnnaBeatrizSilva2904 <annabeatryz2504@gmail.com>
2025-04-07 12:02:04 -0300

    Criando as Models

--- VestLivros/Models/Faculdade.cs
+++ VestLivros/Models/Faculdade.cs
@@ -1,12 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
-namespace VestLivros.Models
+namespace VestLivros.Models;
+
+[Table ("faculdade")]
+public class Faculdade
 {
-    public class Faculdade
-    {
-        
-    }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength (100, ErrorMessage = "Informe a faculdade! (pode ser somente Unicamp ou USP)")]
+    public string FaculdadeNome { get; set; }
 }
