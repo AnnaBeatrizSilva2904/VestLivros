b03efedcd6d30da62b73ee68b3fca5bba6323055
AnnaBeatrizSilva2904 <annabeatryz2504@gmail.com>
2025-06-11 12:05:24 -0300

    Programando a index usuário

---
+++ VestLivros/ViewModels/LoginVM.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+namespace VestLivros.ViewModels;
+
+public class LoginVM
+{
+    [Display(Name = "Email ou nome de usuário", Prompt = "Informe seu Email ou nome de usuário")]
+    [Required(ErrorMessage = "Por favor, informe seu email ou nome de usuário")]
+    public string Email { get; set; }
+
+    [Display(Name = "Senha de Acesso", Prompt = "************")]
+    [Required(ErrorMessage = "Por favor, informe sua senha")]
+    [DataType(DataType.Password)]
+    public string Senha { get; set; }
+
+    [Display(Name = "Manter conectado?")]
+    public bool Lembrar { get; set; }
+
+    public string UrlRetorno { get; set; }        
+}
