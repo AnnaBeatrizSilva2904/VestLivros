84952ab2c57556fd07ebf88b7a122a05576aab16
AnnaBeatrizSilva2904 <annabeatryz2504@gmail.com>
2025-10-13 10:47:58 -0300

    Criando o filtro dos botões

---
+++ VestLivros/ViewModels/LivroFaculdade.cs
@@ -0,0 +1,9 @@
+using VestLivros.Models;
+
+namespace VestLivros.ViewModels;
+
+public class LivroFaculdade
+{
+    public Livro Livro{ get; set; }
+    public Faculdade FaculdadeNome { get; set;}
+}
